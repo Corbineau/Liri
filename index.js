567ebc3deb8fd330a793eae30a824a7ccc79c1f5
Rabbit <Corbineau@users.noreply.github.com>
2019-05-07 12:25:15 -0400

    let's see what happens

--- index.js
+++ index.js
@@ -1,8 +1,11 @@
+
 require("dotenv").config();
 var keys = require("./keys.js");
 var axios = require("axios");
 var spotify = require("node-spotify-api");
 var inquirer = require("inquirer");
+var moment = require('moment');
+var fs = require("fs");
 
 
 
@@ -19,16 +22,31 @@ inquirer.prompt([
                 message: "Song Information",
                 name: "spotify-this-song",
             },
-            { message: "Movie Information" },
-            { name: "Do-what-it-says!" },
-            { message: "Movie Information" },
-            { name: "Do-what-it-says!" }]
+            {
+                message: "Movie Information",
+                name: "movie-this"
+            },
+            {
+                message: "Do What It Says!",
+                name: "Do-what-it-says"
+            }],
         name: "source"
 
     }
 
 ]).then(function (response, err) {
-    if(response.source.name === "concert-this") {
+    if (response.source.name === "concert-this") {
+        inquirer.prompt([
+            {
+                type: "input",
+                message: "What band would you like to see?",
+                name: "band"
+            }
+        ]).then(function (response) {
+            var bandName = response.band;
+            findIt.concertFind(bandName);
+        
+        })
 
     } else if (response.source.name === "spotify-this-song") {
         inquirer.prompt([
@@ -36,32 +54,87 @@ inquirer.prompt([
                 type: "input",
                 message: "What song are you searching for?",
                 name: "song"
-            }        
-        ]).then(function(response) {
+            }
+        ]).then(function (response) {
             var songName = response.song;
-            spotify.search({ type: `track`, query: `${songName}` }, function (err, data) {
-                if (err) {
-                    console.log(`Error occurred: ${err}`);
-                    return;
-                }
-            console.log(data);
-                
-            });
+            findIt.songFind(songName);
 
         })
-    } else if ()
+    } else if (response.source.name === "movie-this") {
+        inquirer.prompt([
+            {
+                type: "input",
+                message: "What movie are you searching for?",
+                name: "movie"
+            }
+        ]).then(function (response) {
+            var movieName = response.movie;
+            findIt.movieFind(movieName);
+
+        })
+    } else {
+        fs.readFile("random.txt") 
+            findIt.songFind();
+        
+    }
 
 })
 
 
 
-var findInAxios = (source, ) => {
-    if (something);
+var findIt = {
+    
+    concertFind: function(bandName) {
+        axios.get(`https://rest.bandsintown.com/artists/${bandName}events?app_id=codingbootcamp`, function (err, data) {
+                if (err) {
+                    console.log(`Error occurred: ${err}`);
+                    return;
+                }
+                console.log(data);
+                fs.appendFile("log.txt", data, function(err) {
+                    if (err) throw err;
+    
+                });
+
+            });
+
+    },
+    
+    movieFind: function(movieName) {
+        axios.get(`http://www.omdbapi.com/?t=${movieName}&y=&plot=short&apikey=trilogy`, function (err, data) {
+                if (err) {
+                    console.log(`Error occurred: ${err}`);
+                    return;
+                }
+                console.log(data);
+                fs.appendFile("log.txt", data, function(err) {
+                    if (err) throw err;
+    
+                });
+
+            });
+
+    },
+
+    songFind: function(songName) {
+        spotify.search({ type: `track`, query: `${songName}` }, function (err, data) {
+            if (err) {
+                console.log(`Error occurred: ${err}`);
+                return;
+            }
+            console.log(data);
+            fs.appendFile("log.txt", data, function(err) {
+                if (err) throw err;
+
+            });
+
+        });
+    },
+    
+
 
 }
 
-
-axios.get(url)
 
 
 
